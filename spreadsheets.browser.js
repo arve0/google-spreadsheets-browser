99e12676a4da204fc635dca5ea16f8a6038acca1
arve0 <arve.seljebu@gmail.com>
2015-11-17 22:39:53 +0100

    build request(opts, callback)

--- spreadsheets.browser.js
+++ spreadsheets.browser.js
@@ -1,10 +1,11 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Spreadsheets = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (process){
+(function (process,global){
 "use strict";
 
 var request = require("request");
 var statuses = require("statuses");
 var querystring = require("querystring");
+global.request = request;
 
 // If caller has installed googleapis, we do some sanity checking to make sure it's a version we know.
 try {
@@ -83,7 +84,7 @@ var getFeed = function(params, auth, query, cb) {
   };
 
   if (!auth) {
-    request.get(options, reqCallback);
+    request(options, reqCallback);
   } else {
     auth.request(options, function(err, body, response) {
       // For some crazy reason, googleapis DefaultTransporter swaps the ordering of the args...
@@ -301,7 +302,7 @@ Spreadsheets.cells = function(opts, cb) {
   });
 };
 
-}).call(this,require('_process'))
+}).call(this,require('_process'),typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"_process":3,"querystring":6,"request":2,"semver":7,"statuses":9}],2:[function(require,module,exports){
 // Browser Request
 //
